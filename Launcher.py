831b408cdf50e157424750b503e8b60128749255
Ender <120225426+RebornEnder@users.noreply.github.com>
2024-04-10 22:05:32 +0200

    Add files via upload

---
+++ Launcher.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import os
+
+Github = "https://raw.githubusercontent.com/RebornEnder/screenblox/main/"
+DataPath = os.getcwd() + "\\ScreenshareData"
+Version = f"{DataPath}\\Version.json"
+AppPath = f"{DataPath}\\App.py"
+
+def main():
+    print("Screenshare Launcher")
+    print("[~] Checking for updates...")
+    try:
+        newversion = requests.get(Github + "Data/Version.json")
+        if not os.path.exists(DataPath):
+            print("[+] First launch detected, downloading files...")
+            os.mkdir(DataPath)
+            newapp = requests.get(Github + "Data/App.py")
+            with open(AppPath, mode="xb") as file:
+                file.write(newapp.content)
+            with open(Version, mode="xb") as file:
+                file.write(newversion.content)
+            print("[+] Launching...\n")
+            exec(open(AppPath).read())
+        elif newversion.json()["Version"] < json.load(open(Version))["Version"] + 0.1:
+            print("[+] Up to date, launching...\n")
+            exec(open(AppPath).read())
+        else:
+            print("[-] Out of date, updating...")
+            newapp = requests.get(Github + "Data/App.py")
+            with open(AppPath, mode="wb") as file:
+                file.write(newapp.content)
+            with open(Version, mode="wb") as file:
+                file.write(newversion.content)
+            print("[+] Launching...\n")
+            exec(open(AppPath).read())
+    except Exception as e:
+        print("[-] Update checking/downloading failed!\n", e)
+        exec(open(AppPath).read())
+
+if __name__ == "__main__":
+    main()
